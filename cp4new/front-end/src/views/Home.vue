25d5f0e0bd907997d8e7bf5ed1dae193b9b98f94
davidwinnbigvideo <71191195+davidwinnbigvideo@users.noreply.github.com>
2021-04-07 19:54:51 -0600

    Update Home.vue

--- cp4new/front-end/src/views/Home.vue
+++ cp4new/front-end/src/views/Home.vue
@@ -25,13 +25,9 @@ export default {
   },
   methods: {
     async getItems() {
-      try {
         let response = await axios.get("/api/items");
         this.items = response.data;
         return true;
-      } catch (error) {
-        console.log(error);
-      }
     },
   }
 }
