b01594e085dd83c87aadd85929a184127c59b4fa
davidwinnbigvideo <71191195+davidwinnbigvideo@users.noreply.github.com>
2021-04-07 19:54:21 -0600

    Update Admin.vue

--- cp4new/front-end/src/views/Admin.vue
+++ cp4new/front-end/src/views/Admin.vue
@@ -81,7 +81,6 @@ export default {
       this.file = event.target.files[0]
     },
     async upload() {
-      try {
         const formData = new FormData();
         formData.append('photo', this.file, this.file.name)
         let r1 = await axios.post('/api/photos', formData);
@@ -91,35 +90,23 @@ export default {
           description: this.description
         });
         this.addItem = r2.data;
-      } catch (error) {
-        console.log(error);
-      }
     },
     async getItems() {
-      try {
         let response = await axios.get("/api/items");
         this.items = response.data;
         return true;
-      } catch (error) {
-        console.log(error);
-      }
     },
     selectItem(item) {
       this.findTitle = "";
       this.findItem = item;
     },
     async deleteItem(item) {
-      try {
         await axios.delete("/api/items/" + item._id);
         this.findItem = null;
         this.getItems();
         return true;
-      } catch (error) {
-        console.log(error);
-      }
     },
     async editItem(item) {
-      try {
         await axios.put("/api/items/" + item._id, {
           title: this.findItem.title,
           description: this.findItem.description,
@@ -127,9 +114,6 @@ export default {
         this.findItem = null;
         this.getItems();
         return true;
-      } catch (error) {
-        console.log(error);
-      }
     },
   }
 }
